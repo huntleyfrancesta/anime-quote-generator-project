a64372e66243a7a47b093ea787a5f38cac023350
huntleyfrancesta <huntleyfrancesta@gmail.com>
2021-04-24 21:57:27 -0400

    done

--- index.js
+++ index.js
@@ -8,50 +8,45 @@ const makeQuoteDiv = (quote) => {
     const p = document.createElement('p')
     const h3 = document.createElement('h3')
     const h4 = document.createElement('h4')
-
     h3.textContent = quote.anime
     p.textContent = quote.quote
     h4.textContent = quote.character
-
     div.appendChild(h3)
     div.appendChild(p)
     div.appendChild(h4)
-
     const quoteContainer = document.getElementById('quote-container');
     quoteContainer.innerHTML = ""
-    quoteContainer.appendChild(div);
-
+        // quoteContainer.appendChild(div);
+    return div
 }
-
-getQuote().then(data => {
-    console.log("fetching...");
-    console.log(data);
-});
-getQuote().then(quote => { makeQuoteDiv(quote) })
-
-const appendChild = (quoteDiv) => {
-    const quoteContainer = document.getElementById('quote-Container')
-    quoteContainer.appendChild(quoteDiv)
-}
-
-const handleFetchError = () => {
-    const quoteContainer = document.getElementById('quote-container ')
-    const warning = document.createElement('p')
-
-    warning.textContent = "something went wrong here..."
-
-    quoteContainer.append(warning)
-}
-
+const appendElement = (quoteDiv) => {
+        const quoteContainer = document.getElementById('quote-container')
+        quoteContainer.appendChild(quoteDiv)
+    }
+    // getQuote().then(data => {
+    //     console.log("fetching...");
+    //     console.log(data);
+    // });
+    // const handleFetchError = () => {
+    //     const quoteContainer = document.getElementById('quote-container ')
+    //     const warning = document.createElement('p')
+    //     warning.textContent = "something went wrong here..."
+    //     quoteContainer.append(warning)
+    // }
 const generateQuote = () => {
     getQuote().then((quote) => {
+            console.log(quote)
             const quoteDiv = makeQuoteDiv(quote)
-            appendChild(quoteDiv)
+            appendElement(quoteDiv)
         })
-        .catch(handleFetchError)
+        // .catch(handleFetchError)
 }
-
-getQuote().then(quote => {
-    const quoteDiv = makeQuoteDiv(quote)
-    console.log(quoteDiv)
+generateQuote()
+    // getQuote().then(quote => {
+    //     const quoteDiv = makeQuoteDiv(quote)
+    //     console.log(quoteDiv)
+    // })
+document.getElementById('nextquote').addEventListener('click', (e) => {
+    e.preventDefault();
+    generateQuote();
 })
